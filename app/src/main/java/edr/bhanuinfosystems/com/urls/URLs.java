ee1b8d67ad1e55daf1da74e6bb4379830c834e00
gowtham <gurralagowtham@gmail.com>
2019-05-07 16:25:18 +0530

    all files

--- app/src/main/java/edr/bhanuinfosystems/com/urls/URLs.java
+++ app/src/main/java/edr/bhanuinfosystems/com/urls/URLs.java
@@ -1,13 +1,13 @@
 package edr.bhanuinfosystems.com.urls;
 public class URLs {
 
-    private static final String ROOT_URL = "http://192.168.43.39/backend/admin_log";
-    public static final String URL_DOC_REG="http://192.168.43.39/backend/doc.php";
-    public static final String URL_DOC_patlist="http://192.168.43.39/backend/doc_pat.php";
-    public static final String URL_ADD_PAT="http://192.168.43.39/backend/add_pat.php";
-    public static final String URL_DOC_LOGIN= "http://192.168.43.39/backend/doc_login.php";
-    public static final String URL_doclist = "http://192.168.43.39/backend/doclist.php";
-    public static final String URL_admin_login ="http://192.168.43.39/backend/admin_login.php";
+    private static final String ROOT_URL = "http://54.184.93.137/backend/admin_log";
+    public static final String URL_DOC_REG="http://54.184.93.137/backend/doc.php";
+    public static final String URL_DOC_patlist="http://54.184.93.137/backend/doc_pat.php";
+    public static final String URL_ADD_PAT="http://54.184.93.137/backend/add_pat.php";
+    public static final String URL_DOC_LOGIN= "http://54.184.93.137/backend/doc_login.php";
+    public static final String URL_doclist = "http://54.184.93.137/backend/doclist.php";
+    public static final String URL_admin_login ="http://54.184.93.137/backend/admin_login.php";
 }
 
 
